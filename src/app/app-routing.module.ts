2b303771b6bf2bef7f82c714552a28d2608154a1
Sai Madhuri Kasam <saimadhuri.kasam@valuemomentum.com>
2022-08-01 18:10:50 +0530

    Committing Components and Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListUsersComponent } from './list-users/list-users.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { ListTaskComponent } from './list-task/list-task.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'register', component:RegisterComponent},
+  {path:'login', component:LoginComponent},
+  {path:'listuser', component:ListUsersComponent},
+  {path:'addtask', component:AddTaskComponent},
+  {path:'listtask',component:ListTaskComponent},
+  {path:'', redirectTo:'home' ,pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ListUsersComponent } from './list-users/list-users.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { EditTaskComponent } from './edit-task/edit-task.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    ListUsersComponent,
+    AddTaskComponent,
+    HeaderComponent,
+    HomeComponent,
+    EditTaskComponent,
   ],
   imports: [
     BrowserModule,
